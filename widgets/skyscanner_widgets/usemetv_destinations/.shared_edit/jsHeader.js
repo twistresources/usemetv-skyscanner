22d69f24d5707808e575c4c1a6814fc46aed2324
Philip Callender <philcal@mac.com>
2015-09-09 21:36:26 -0400

    Converted skyscanner pages to group editing

---
+++ widgets/skyscanner_widgets/usemetv_destinations/.shared_edit/jsHeader.js
@@ -0,0 +1,89 @@
+// Initialize the UseMe display
+$(document).ready(function() {
+    
+    // Check that Shywall got loaded.
+    // If not, wait a while and reload the page
+    if (typeof(Shywall) == 'undefined') {
+        console.log('Could not load Shywall');
+        var waitTime = 20 * 1000;
+        setTimeout(function() {
+              window.location.reload();
+            }, waitTime);
+        return;
+    }
+
+    // Get the controls for the page
+    var homePage = '/';    
+    var controls = [ ];
+    
+    // Add the destinations first
+    $('.usemetv-destination').each(function(index, dst){
+        var $dst = $(dst);
+        var airportCode = $dst.attr('data-airportCode');
+        var description = $dst.attr('data-description');
+        controls.push({
+            label: description,
+            onClick: function(){
+                PricesByDate.setDestination(airportCode, description)
+            }
+        });
+        
+        if (index == 0) PricesByDate.setDestination(airportCode, description);
+    });
+    
+    // Now add the regular buttons
+    var $buttons = $(".usemetv-button");
+    $buttons.each(function(index, button){
+        var $button = $(button)
+        var label = $button.attr('data-label');
+        var ref = $button.attr('data-ref');
+        var home = $button.attr('data-home');
+        if (home) {
+            
+            // Remember this as the home page
+            homePage = ref;
+        } else {
+            
+            // Add as a Button on the Remote
+            controls.push({
+                label: label,
+                onClick: ref
+            });            
+        }
+        
+    });
+console.log('controls=', controls)    
+    
+    // Initialize Useme.tv
+	var config = {
+		wallKey: USEMETV_WALLKEY,
+		menuLabel: 'Destination Prices',
+		home: homePage,
+		controls: controls,
+		shywallHost: USEMETV_HOST,
+		shywallPort: USEMETV_PORT
+	};
+    
+    /*
+    // Add controls defined by the page
+    var $buttons = $(".usemetv-button");
+    $buttons.each(function(index, button){
+        var $button = $(button)
+        var label = $button.attr('data-label');
+        var ref = $button.attr('data-ref');
+        config.controls.push({
+            label: label,
+            onClick: ref
+        });
+    });
+    
+    // Add a back button
+    config.controls.push({
+        label: 'Back',
+        onClick: backPage
+    });
+    */
+    
+    Shywall.shywall(config);
+
+});
